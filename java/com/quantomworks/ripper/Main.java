85ab0f37c2d8cbc7a68266cb06d0555ecb5f9352
Samuel Walker <sfxworks@gmail.com>
2016-06-01 06:15:34 -0400

    Iterator / Prep
    Added a loop to cycle through existing blocks and items.

--- java/com/quantomworks/ripper/Main.java
+++ java/com/quantomworks/ripper/Main.java
@@ -1,10 +1,15 @@
 package com.quantomworks.ripper;
 
-import net.minecraft.init.Blocks;
+import java.util.Iterator;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.common.registry.IForgeRegistry;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main
@@ -15,10 +20,26 @@ public class Main
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		/* Some for each statement cycling through a registry, getting what I'm hoping is a Block class
-    	   that has properties like .image (bitmap, which I assume would be an array given blocks have 6 faces)
-    	   .name, .recipe, and */ 
+        System.out.println("Waiting for other mods..");
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    	System.out.println("Scanning for new items..");
     	
-        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+    	for (Iterator<Block> i = ForgeRegistries.BLOCKS.iterator(); i.hasNext(); )
+    	{
+    		Block block = i.next();
+    		System.out.println("New block found:" + block.getLocalizedName());
+    		//A bunch of other cool things
+    	}
+    	for (Iterator<Item> j = ForgeRegistries.ITEMS.iterator(); j.hasNext(); )
+    	{
+    		Item item = j.next();
+    		System.out.println("New item found:" + item.getUnlocalizedName());
+    		//A bunch of other cool things
+    	}
+    	
     }
 }
